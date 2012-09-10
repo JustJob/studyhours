8cb4640a6858e066d873e531559a8e7e63fdd7d9
Justin O'Brien <justinobrien07@gmail.com>
2012-09-09 23:43:11 -0500

    adding initial files

---
+++ hoursheet/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+  firstName = models.CharField(max_length=25)
+  lastName = models.CharField(max_length=30)
+  weeklyHours = models.IntegerField(default=12)
+  currentFine = models.FloatField(default = 2.0)
+  signedIn = models.BooleanField(default=True)
+
+  def __unicode__(self):
+    return self.firstName + " " + self.lastName
+
+  def getCurrentHours(startTime, endTime):
+    retval = 0.0;
+    for(time in TimingEvent.objects.filter(person__exact=self, 
+                                           signedIn__gte=startTime, 
+                                           signedOut__lte=endTime)):
+
+    return 0
+
+class TimingEvent(models.Model):
+  person = models.ForeignKey(Person)
+  signedIn = models.DateField(auto_now_add=True)
+  signedOut = models.DateField(null=True)
+
+  def timeDifference():
+    if(signedOut)
+
+  def __unicode__(self):
+    return self.person + " TimingEvent"
+
