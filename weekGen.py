4e14eeecc9afb2a22b7540d24b0665767d73d3f0
Justin O'Brien <justinobrien07@gmail.com>
2012-10-29 23:55:19 -0500

    added person generation to week generation and removed width setting on main div

--- weekGen.py
+++ weekGen.py
@@ -18,7 +18,22 @@ end = end + datetime.timedelta(days=7)
 for x in xrange(15):
   print "now is ", now
   print "end is ", end
+  week = Week(start=now, end=end)
   now = now + datetime.timedelta(days=7)
   end = end + datetime.timedelta(days=7)
-  week = Week(start=now, end=end)
   week.save()
+
+people = ['Adam Kosecki', 'Alex Delong', "Alex O'Neil", 'Andrew Baumen', 'Bret Payne', 'Cody Burgess', 'Cody Haislip', 'Corey Reuter', 'Dylan Alarcon', 'Gage Witt', 'Grant Luebbering', 'Jacob Francka', 'Jake Kistler', 'John Walsh', 'Joel Cardin', 'Jerek Nash', 'Josiah Enns', 'Kevin Walaszek', 'Lafe Windmiller', 'Logan Gesiler', 'Matt Achelpohl', 'Michael Keener', 'Ollie Naeger', 'Trevor Townsend', 'Brandon Gasser', 'Sam Manson', "Jimmy O'Donnel", 'Jeremy Satterfield', 'Josh Weber']
+for person in people:
+  thisperson = Person(firstName=person.split(' ')[0], lastName=person.split(' ')[1], weeklyHours=12, currentFine=2)
+  thisperson.save()
+
+people = ['Ryan Covington', 'Daniel FLing', 'Nick Slama', 'Kenny Holtz', 'Mason Smith']
+for person in people:
+  thisperson = Person(firstName=person.split(' ')[0], lastName=person.split(' ')[1], weeklyHours=6, currentFine=2)
+  thisperson.save()
+
+people = ['Eric Moore', 'Tiredej Bunyarataphantusadf', 'Brian Bakala', 'Jared Wyatt', 'Zach Boswell', 'Mark Bradshaw', 'Alex Miles']
+for person in people:
+  thisperson = Person(firstName=person.split(' ')[0], lastName=person.split(' ')[1], weeklyHours=7, currentFine=2)
+  thisperson.save()
