d583c25b6aebdfb8d0498e5219dc85eebeae7ba3
Justin O'Brien <justinobrien07@gmail.com>
2012-10-29 23:24:02 -0500

    adding week generation utility

---
+++ weekGen.py
@@ -0,0 +1,20 @@
+#generates weeks for study hours
+
+import datetime
+from hoursheet.models import Person, Week, TimingEvent
+import pytz
+
+today = datetime.date.today()
+dayoffset = today.weekday()
+now = datetime.datetime(today.year, today.month, today.day,tzinfo=pytz.timezone('US/Central'))
+now = now + datetime.timedelta(days = -1 * (dayoffset + 1))
+end = datetime.datetime(now.year,now.month,now.day,23,59,59, tzinfo=pytz.timezone('US/Central'))
+end = end + datetime.timedelta(days=7)
+
+for x in xrange(15):
+  print "now is ", now
+  print "end is ", end
+  now = now + datetime.timedelta(days=7)
+  end = end + datetime.timedelta(days=7)
+  week = Week(start=now, end=end)
+  week.save()
